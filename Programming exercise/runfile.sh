57c2c04e328a689fdd76f67162fbb16efc022202
ZsofiaK <113365620+ZsofiaK@users.noreply.github.com>
2024-01-08 11:51:36 +0100

    Update runfile.sh

--- Programming exercise/runfile.sh
+++ Programming exercise/runfile.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-#SBATCH --gpus=1
-#SBATCH --partition=gpu
-#SBATCH --time=14:00:00
-#SBATCH -o exercise_%j.out
+!/bin/bash
+SBATCH --gpus=1
+SBATCH --partition=gpu
+SBATCH --time=14:00:00
+SBATCH -o exercise_%j.out
 
 echo "gpus $SLURM_GPUS on node: $SLURM_GPUS_ON_NODE"
 echo "nodes nnodes: $SLURM_NNODES, nodeid: $SLURM_NODEID, nodelist $SLURM_NODELIST"
